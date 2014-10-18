71c68697163e98a1b72a87a9a729306b87d5c239
Rushabh Padalia <padalia.rushabh@gmail.com>
2014-10-18 14:41:41 +1100

    now sends email

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from gi.repository import Gtk
+from easymail_lib import Email
 
 
 class Handler(object):
@@ -6,7 +7,17 @@ class Handler(object):
     Gtk.main_quit(*args)
 
   def on_btn_Send_clicked(this, *args):
-    print("Hello")
+    email.set_to_address(email_to.get_text())
+    email.set_from_address("padalia.rushabh@gmail.com")
+
+    email.set_email_subject(email_subject.get_text())
+    email.set_email_server("smtp.gmail.com:587")
+    email.set_username_and_password("padalia.rushabh@gmail.com" , "Creatives@321.com")
+
+    text_buffer = email_body.get_buffer()
+    email.set_email_body(text_buffer.get_text(text_buffer.get_start_iter() ,text_buffer.get_end_iter(), True))
+    email.send_email()
+    print("Mail Send")
 
 
 
@@ -16,7 +27,13 @@ builder.connect_signals(Handler())
 window = builder.get_object("window_easymail")
 window.show_all()
 
+#Email TextBox
+email_to = builder.get_object("txt_email_to")
+email_subject = builder.get_object("txt_email_subject")
 email_body = builder.get_object("txt_email_body")
+
+
+email = Email.EasyMail()
 
 
 Gtk.main()
